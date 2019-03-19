67e86a2c0b26c62fe4f83d866224fbb0d1b9c32a
zylee <zylee@ymail.com>
2019-03-19 14:49:29 +0800

    Changed Values to float

--- Alex/Alex.ino
+++ Alex/Alex.ino
@@ -6,14 +6,14 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  forward(100.0);
-  delay(500);
+  forward(1000.0);
+  delay(500.0);
   left(100.0);
-  delay(500);
+  delay(500.0);
   backward(100.0);
-  delay(500);
+  delay(500.0);
   right(100.0);
-  delay(500);
+  delay(500.0);
   stop();
-  delay(2000);
+  delay(2000.0);
 }
